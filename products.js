718e6a2e9350290b030e0c283ce8b84ad8d080f5
xrmz.v3 <132705514+Abdoch333@users.noreply.github.com>
2026-05-20 17:53:55 +0100

    Add files via upload

--- products.js
+++ products.js
@@ -5,7 +5,9 @@ const products = [
         category: "shoes",
         price: 1900,
         images: [
-            "product_image.jpg"
+            "https://hsxmoiojpzgjdtpprylz.supabase.co/storage/v1/object/public/images/8fe1ec5a-9d8f-4221-8749-58a8c7cd7d5e.jpg",
+            "https://hsxmoiojpzgjdtpprylz.supabase.co/storage/v1/object/public/images/0579f3c3-c03e-4259-addf-04d6ef501979.jpg",
+            "https://hsxmoiojpzgjdtpprylz.supabase.co/storage/v1/object/public/images/1b72631e-e888-41f6-b5ce-771517446396.jpg"
         ],
         tag: "جديد",
         type: "new",
